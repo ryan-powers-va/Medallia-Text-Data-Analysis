dc2c9068a7056548f0f834715d52afc64568e181
ryan-powers-va <ryan.powers3@va.gov>
2025-01-06 14:55:24 -0500

    Updates.

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 # Predefined tags
 TAGS = [
-    "Triage Group", "Unrelated to VA.gov", "Error", "Integration", "Ease of use",
+    "Triage Group", "Unrelated to VA.gov", "Bug", "Integration", "Ease of use",
     "Benefits", "Feature Request", "Supplies", "Other", "Mixed Status", "Can't Reply",
     "Early pop up", "Missing Rx", "Findability/ Navigation", "Sign in or access",
     "Content", "Sort", "Answered Question", "Can't Refill", "Page Length"
